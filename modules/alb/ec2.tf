7700f4ff1d1f22c1a83e4e8043906ae0208b25d2
Nick Ciraki <nicholas.ciraki@jdplc.com>
2023-10-23 15:21:51 +0100

    changed AMI data source to Amazon Lin

--- modules/alb/ec2.tf
+++ modules/alb/ec2.tf
@@ -1,12 +1,12 @@
-# data "aws_ssm_parameter" "amzn2_linux" {
-#   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-# }
+data "aws_ssm_parameter" "amzn2_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
 
 # INSTANCES #
 resource "aws_instance" "ec2" {
   count = var.instance_count
-  # ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  ami           = "ami-067d1e60475437da2"
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  # ami           = "ami-067d1e60475437da2"
   instance_type = var.instance_type
   # subnet_id = var.subnets[(count.index % var.vpc_public_subnet_count)].id
   subnet_id              = element(var.subnets.*, count.index)
